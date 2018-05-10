0ed27f1e42281d7514b9e2701a7f06dcee7b6a0b
Kamilah Amir-Al-Sharif <kamilahamir-al-sharif@Kamilahs-MacBook-Pro.local>
2018-05-09 21:34:10 -0400

    hover and stars css

---
+++ about.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $("#carlyimg ").hover(
+        function () {
+            $(this).attr("src", "./img/bitmoji.jpg");
+        },
+        function () {
+            $(this).attr("src", "./img/carly.jpg");
+        }
+    );
+})
+
+$(document).ready(function(){
+    $("#stacyimg ").hover(
+        function () {
+            $(this).attr("src", "./img/bitmoji.jpg");
+        },
+        function () {
+            $(this).attr("src", "./img/stacy.jpg");
+        }
+    );
+})
+
+
+$(document).ready(function(){
+    $("#krissyimg ").hover(
+        function () {
+            $(this).attr("src", "./img/bitmoji.jpg");
+        },
+        function () {
+            $(this).attr("src", "./img/krissy.jpg");
+        }
+    );
+})
+
+$(document).ready(function(){
+    $("#kamimg ").hover(
+        function () {
+            $(this).attr("src", "./img/bitmoji.jpg");
+        },
+        function () {
+            $(this).attr("src", "./img/kam.jpg");
+        }
+    );
+})
